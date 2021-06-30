744e596376acd044d3415c84713a4921d6f952b3
shloimiminkowicz <shloimimink@gmail.com>
2021-06-30 11:02:21 -0500

    removed last name from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm
-                    Shloimi Mink</h1>
+                    Shloimi</h1>
             </section>
         </main>
     );
